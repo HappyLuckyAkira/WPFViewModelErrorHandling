a3dd7b8dcede795ba63fe47ab057982ac47b91f8
Akira Kubo <akira.kubo@konicaminolta.com>
2018-04-17 14:46:03 +0900

    ﻿:+1: 機能改善
    ひとまず、赤枠が出るところまで
    
    (cherry picked from commit de1ad9395f41e99dcea215bb003d410a02dc3e27)

--- WpfApp1/MainWindowViewModel.cs
+++ WpfApp1/MainWindowViewModel.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Prism.Commands;
+using Prism.Mvvm;
+//以下参照
+//http://blog.okazuki.jp/entry/20110118/1295338167
+//https://qiita.com/hiki_neet_p/items/e381c687b0644c0e4978
 namespace WpfApp1
 {
-    class MainWindowViewModel
+
+    class MainWindowViewModel: BindableBase
     {
+        private int input;
+        private int input2;
+
+
+        public MainWindowViewModel()
+        {
+            this.SampleCommand = new DelegateCommand(
+                // Execute
+                () =>
+                {
+                },
+                // CanExecute
+                () =>
+                {
+                    // 入力エラーがあったらfalseを返したいよね
+                    return true;
+                });
+        }
+
+    public DelegateCommand SampleCommand { get; private set; }
+
+        public int Input
+        {
+            get
+            {
+                return this.input;
+            }
+
+            set
+            {
+                SetProperty(ref input, value);
+            }
+        }
+
+        public int Input2
+        {
+            get
+            {
+                return this.input2;
+            }
+
+            set
+            {
+                SetProperty(ref input2, value);
+            }
+        }
     }
 }
